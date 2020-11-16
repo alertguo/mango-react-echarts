59e72009501df4a96f5b09338e69ea54312949b3
alertguo <1196614612@qq.com>
2020-11-16 20:41:11 +0800

    添加 React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Redirect exact from="/" to="money" />
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Tags() {
+  return <h2>标签</h2>;
+}
+
+function Money() {
+  return <h2>记账</h2>;
+}
+
+function Statistics() {
+  return <h2>统计</h2>;
+}
+function NoMatch() {
+  return <h2>该页面不存在</h2>;
 }
 
 export default App;
