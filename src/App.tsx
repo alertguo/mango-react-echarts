ebefc6f65bfc7e6d790ce6dbb8d85d2939743704
alertguo <1196614612@qq.com>
2020-11-17 15:51:39 +0800

    去掉404页面的Nav

--- src/App.tsx
+++ src/App.tsx
@@ -23,40 +23,56 @@ const Main = styled.div`
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Redirect exact from="/" to="money"/>
-            <Route path="/label">
-              <Label/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Redirect exact from="/" to="money"/>
+        <Route path="/label">
+          <Label/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Route path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 function Label() {
-  return <h2>标签</h2>;
+  return (
+    <Wrapper>
+      <Main>
+        <h2>标签</h2>
+      </Main>
+      <Nav/>
+    </Wrapper>
+  );
 }
 
 function Money() {
-  return <h2>记账</h2>;
+  return (
+    <Wrapper>
+      <Main>
+        <h2>记账</h2>
+      </Main>
+      <Nav/>
+    </Wrapper>
+  );
 }
 
 function Statistics() {
-  return <h2>统计</h2>;
+  return (
+    <Wrapper>
+      <Main>
+        <h2>统计</h2>
+      </Main>
+      <Nav/>
+    </Wrapper>
+  );
 }
 
 function NoMatch() {
