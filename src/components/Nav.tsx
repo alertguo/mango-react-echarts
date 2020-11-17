9db0793a9c5a04d6cdb1040ae512d3f1bcdbb980
alertguo <1196614612@qq.com>
2020-11-17 17:08:45 +0800

    React Router 添加 activeClassName

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
@@ -21,6 +21,12 @@ const NavWrapper = styled.div`
           width: 24px;
           height: 24px;
         }
+        &.selected {
+          color: #ffda44;
+          .icon {
+            fill: #ffda44;
+          }
+        }
       }
     }
   }
@@ -30,22 +36,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/label">
+          <NavLink to="/label" activeClassName="selected">
             <Icon name="label"/>
             标签
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/money">
+          <NavLink to="/money" activeClassName="selected">
             <Icon name="money"/>
             记账
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/statistics">
+          <NavLink to="/statistics" activeClassName="selected">
             <Icon name="statistics"/>
             统计
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
