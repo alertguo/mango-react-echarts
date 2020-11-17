9e1d34b9f7757a2c08724a77c472316fa03a61ed
alertguo <1196614612@qq.com>
2020-11-17 16:21:45 +0800

    创建 views 目录放几个主要的组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import 'index.scss';
-import Layout from './components/Layout';
+import Money from './views/Money';
+import Label from './views/Label';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 
 function App() {
@@ -31,32 +29,5 @@ function App() {
   );
 }
 
-function Label() {
-  return (
-    <Layout>
-      <h2>标签</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账</h2>
-    </Layout>
-  );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计</h2>
-    </Layout>
-  );
-}
-
-function NoMatch() {
-  return <h2>该页面不存在</h2>;
-}
 
 export default App;
