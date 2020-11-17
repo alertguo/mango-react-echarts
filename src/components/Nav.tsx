37e4a3aa6b3205b6dae85309aad42502a9fe5d9d
alertguo <1196614612@qq.com>
2020-11-17 15:42:55 +0800

    封装 Icon 组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-require('icons/label.svg');
-require('icons/money.svg');
-require('icons/statistics.svg');
+import Icon from './icon';
 
 const NavWrapper = styled.div`
   line-height: 24px;
@@ -30,21 +28,15 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#label"/>
-          </svg>
+          <Icon name="label"/>
           <Link to="/label">标签</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#money"/>
-          </svg>
+          <Icon name="money"/>
           <Link to="/money">记账</Link>
         </li>
         <li>
-          <svg className="icon">
-            <use xlinkHref="#statistics"/>
-          </svg>
+          <Icon name="statistics"/>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
