64b59931e2dd4383fcc9a3157021e31c1e3abe12
alertguo <1196614612@qq.com>
2020-11-17 17:51:38 +0800

    完成记账页面HTML

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,67 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const TagsSelection = styled.section`
+  border: 1px solid red;
+`;
+const NotesSelection = styled.section`
+  border: 1px solid green;
+`;
+const TypeSelection = styled.section`
+  
+`;
+const NumberPadSelection = styled.section`
+  
+`;
 
 function Money() {
   return (
     <Layout>
-      <h2>记账</h2>
+      <TypeSelection>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </TypeSelection>
+      <TagsSelection>
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
+        <button>
+          新增标签
+        </button>
+      </TagsSelection>
+      <NotesSelection>
+        <label>
+          <span>备注</span>
+          <input type="text"/>
+        </label>
+      </NotesSelection>
+      <NumberPadSelection>
+        <div>
+          100
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </NumberPadSelection>
     </Layout>
   );
 }
