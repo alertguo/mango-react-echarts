d086ec4d2f0e8269a67f32f8289998a67e8502e9
alertguo <1196614612@qq.com>
2020-11-18 16:48:04 +0800

    TagsSelection 模块化

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,35 +1,8 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
+import TagsSelection from './Money/TagsSelection';
 
-const TagsSelection = styled.section`
-  background: #ffffff;
-  padding: 12px 16px;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-  > ol {
-    margin: 0 -12px;
-    > li {
-      background: #d9d9d9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
-    }
-  }
-  > button {
-    background: none;
-    border: none;
-    padding: 2px 4px;
-    border-bottom: 1px solid #333;
-    color: #666;
-    margin-top: 8px;
-  }
-`;
 const NotesSelection = styled.section`
   border: 1px solid green;
 `;
