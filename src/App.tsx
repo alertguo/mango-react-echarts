1d2dc5c0852e0ee20e463ebd85bd639e96db4601
alertguo <1196614612@qq.com>
2020-11-18 16:42:25 +0800

    完成 TagSection 的 UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,33 @@ import Money from './views/Money';
 import Label from './views/Label';
 import Statistics from './views/Statistics';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
 
 
+const AppWrapper = styled.div`
+  color: #333;
+`
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Redirect exact from="/" to="money"/>
-        <Route path="/label">
-          <Label/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Redirect exact from="/" to="money"/>
+          <Route path="/label">
+            <Label/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,7 +3,32 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagsSelection = styled.section`
-  border: 1px solid red;
+  background: #ffffff;
+  padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  > ol {
+    margin: 0 -12px;
+    > li {
+      background: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
 `;
 const NotesSelection = styled.section`
   border: 1px solid green;
