4533dd113c63b39060b3e5a54878734c5d6727f4
alertguo <1196614612@qq.com>
2020-11-23 20:57:23 +0800

    Input 修改为受控组件，方便传入数据

--- src/views/Money/NotesSection.tsx
+++ src/views/Money/NotesSection.tsx
@@ -1,48 +1,36 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
+import {Input} from 'components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  > label {
-    display: flex;
-    align-items: center;
-    > span {
-      margin-right: 4px;
-      white-space: nowrap;
-    }
-    > input {
-      display: block;
-      width: 100%;
-      height: 40px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 type Props = {
   value: string,
   onChange: (value: string) => void
 }
 const NotesSection: React.FC<Props> = (props) => {
-  const note = props.value
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value);
-    }
-  };
+  const note = props.value;
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e)=>{
+    props.onChange(e.target.value)
+  }
+  // const refInput = useRef<HTMLInputElement>(null);
+  // const onBlur = () => {
+  //   if (refInput.current !== null) {
+  //     props.onChange(refInput.current.value);
+  //   }
+  // };
   return (
     <Wrapper>
-      <label>
-        <span>备注：</span>
-        <input type="text" placeholder="点击添加备注"
-               ref={refInput}
-               defaultValue={note}
-               onBlur={onBlur}
-        />
-      </label>
+      <Input label="备注：" type="text" value={note} onChange={onChange} placeholder="点击添加备注"/>
+      {/*<span>备注：</span>*/}
+      {/*<input type="text" placeholder="点击添加备注"*/}
+      {/*       ref={refInput}*/}
+      {/*       defaultValue={note}*/}
+      {/*       onBlur={onBlur}*/}
+      {/*/>*/}
     </Wrapper>
   );
 };
