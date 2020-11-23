ee0d802bc6a1db2774cc9a243b8b05a6123ea5d9
alertguo <1196614612@qq.com>
2020-11-23 21:10:24 +0800

    提取出 Button 的居中和空隙的样式

---
+++ src/components/Center.tsx
@@ -0,0 +1,10 @@
+// 居中
+import styled from 'styled-components';
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+export {Center};
