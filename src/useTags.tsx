b7f4f3beddbb5431c603937630b2910cd7391e7b
alertguo <1196614612@qq.com>
2020-11-24 17:33:09 +0800

    更新数据使用 map 修改 tags 里对应的 id 项

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -22,21 +22,22 @@ const useTags = () => { // 封装一个自定义的 Hook
     return result;
   };
   const updateTag = (id: number, obj: { name: string }) => {
-    // 获取到在 tags 中对应的下标
-    const index = findTagIndex(id);
-    // 深拷贝 tags （React 不推荐改变原数组，如果发现还是原来的对象，会不做任何改变）
-    let tagsClone = JSON.parse(JSON.stringify(tags));
-    // 替换该项数据
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+    // 使用 map 替换深拷贝
+    setTags(tags.map(tag => {
+        return tag.id === id ? {id: id, name: obj.name} : tag;
+      }
+    ));
+    // // 获取到在 tags 中对应的下标
+    // const index = findTagIndex(id);
+    // // 深拷贝 tags （React 不推荐改变原数组，如果发现还是原来的对象，会不做任何改变）
+    // let tagsClone = JSON.parse(JSON.stringify(tags));
+    // // 替换该项数据
+    // tagsClone.splice(index, 1, {id: id, name: obj.name});
+    // setTags(tagsClone);
   };
   const deleteTag = (id: number) => {
     //使用 filter 替代下面的四行代码
     setTags(tags.filter(tag => tag.id !== id));
-    // const index = findTagIndex(id);
-    // let tagsClone = JSON.parse(JSON.stringify(tags));
-    // tagsClone.splice(index, 1);
-    // setTags(tagsClone);
   };
   // 返回接口，不能是返回数组，不然使用就报错
   return {tags, setTags, findTag, findTagIndex, updateTag, deleteTag};
