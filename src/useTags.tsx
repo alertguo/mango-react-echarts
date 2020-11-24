78cc0e16e23846f24fec9f642603e88bebd75891
alertguo <1196614612@qq.com>
2020-11-24 17:33:48 +0800

    删除一行备注

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -36,7 +36,6 @@ const useTags = () => { // 封装一个自定义的 Hook
     // setTags(tagsClone);
   };
   const deleteTag = (id: number) => {
-    //使用 filter 替代下面的四行代码
     setTags(tags.filter(tag => tag.id !== id));
   };
   // 返回接口，不能是返回数组，不然使用就报错
