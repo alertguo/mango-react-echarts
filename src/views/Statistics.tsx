63e223ffd2675c9d0eb4cf99b6bf9ad804565da0
alertguo <1196614612@qq.com>
2020-11-26 19:21:45 +0800

    统计页面添加 type 切换

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,10 +1,18 @@
 import Layout from 'components/Layout';
-import React from 'react';
+import React, {useState} from 'react';
+import TypeSection from './Money/TypeSection';
+import {useRecords} from '../hooks/useRecords';
 
 function Statistics() {
+  const [type, setType] = useState<'-' | '+'>('-');
+  const {records} = useRecords();
   return (
     <Layout>
-      <h2>统计</h2>
+      <TypeSection value={type}
+                   onChange={value => setType(value)}/>
+      <div>
+        {JSON.stringify(records)}
+      </div>
     </Layout>
   );
 }
