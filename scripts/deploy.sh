fd4b8234df0118aa18939b88f6775703885021dd
alertguo <1196614612@qq.com>
2020-11-30 11:19:04 +0800

    添加部署到码云

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,10 @@ cd build
 git init
 git add .
 git commit -m 'deploy'
-git remote add origin git@github.com:alertguo/mango-react-website.git
+# 部署到 GitHub
+ git remote add origin git@github.com:alertguo/mango-react-website.git
+# 部署到码云
+#git remote add origin git@gitee.com:alertguo/mango-react-website.git
 git branch -M main
 git push -u origin main -f
 cd -
