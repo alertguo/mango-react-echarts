6f27f3882e8a5fecab322c668245bed78e30b8ba
alertguo <1196614612@qq.com>
2020-11-30 11:30:59 +0800

    修改部署命令，添加 &&

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-yarn build
-cd build
-git init
-git add .
-git commit -m 'deploy'
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
 # 部署到 GitHub
- git remote add origin git@github.com:alertguo/mango-react-website.git
+ git remote add origin git@github.com:alertguo/mango-react-website.git &&
 # 部署到码云
-#git remote add origin git@gitee.com:alertguo/mango-react-website.git
-git branch -M main
-git push -u origin main -f
+#git remote add origin git@gitee.com:alertguo/mango-react-website.git &&
+git branch -M main &&
+git push -u origin main -f &&
 cd -
