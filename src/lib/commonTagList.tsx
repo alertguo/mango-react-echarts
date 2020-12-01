d54ef6a0a05a16498a7880c5a61ae6e7ecbb44f0
alertguo <1196614612@qq.com>
2020-12-01 17:08:01 +0800

    添加类型

---
+++ src/lib/commonTagList.tsx
@@ -0,0 +1,36 @@
+export type CommonTagList = {
+  index: number
+  name: string
+}
+export const commonTagList: CommonTagList[] = [
+  {index: 0, name: 'commonTag-0'},
+  {index: 1, name: 'commonTag-1'},
+  {index: 2, name: 'commonTag-2'},
+  {index: 3, name: 'commonTag-3'},
+  {index: 4, name: 'commonTag-4'},
+  {index: 5, name: 'commonTag-5'},
+  {index: 6, name: 'commonTag-6'},
+  {index: 7, name: 'commonTag-7'},
+  {index: 8, name: 'commonTag-8'},
+  {index: 9, name: 'commonTag-9'},
+  {index: 10, name: 'commonTag-10'},
+  {index: 11, name: 'commonTag-11'},
+  {index: 12, name: 'commonTag-12'},
+  {index: 13, name: 'commonTag-13'},
+  {index: 14, name: 'commonTag-14'},
+  {index: 15, name: 'commonTag-15'},
+  {index: 16, name: '交通'},
+  {index: 17, name: '医疗'},
+  {index: 18, name: '学习'},
+  {index: 19, name: '宠物'},
+  {index: 20, name: '旅行'},
+  {index: 21, name: '日用'},
+  {index: 22, name: '社交'},
+  {index: 23, name: '购物'},
+  {index: 24, name: '餐饮'},
+  {index: 25, name: '兼职'},
+  {index: 26, name: '工资'},
+  {index: 27, name: '理财'},
+  {index: 28, name: '礼金'},
+  {index: 29, name: '其他'},
+];
